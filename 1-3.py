e2ad4f77e0455f8e5cc7873c57a4e7b4a09686af
alsu198613 <99893979+alsu198613@users.noreply.github.com>
2022-03-23 20:38:40 +0300

    Update 1-3.py

--- 1-3.py
+++ 1-3.py
@@ -6,17 +6,3 @@ n1 = n
 n2 = int(str(n) * 2)
 n3 = int(str(n) * 3)
 print(n1 + n2 + n3)
-
-# nn = n * 10 + n
-# nnn = n * 100 + nn
-# print(n + nn + nnn)
-#
-#
-# def solve(n):
-#    n1 = n
-#    n2 = int(str(n) * 2)
-#    n3 = int(str(n) * 3)
-#    print(n1 + n2 + n3)
-#
-#
-# solve(33)
